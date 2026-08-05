f36e8d15e97642c39d66304e9ed5e6aaa07c5599
Gopikrishna <chodapaneedigopikrishna@gmail.com>
2026-08-05 10:04:29 +0530

    8 -commit

--- springboot-devops-app/src/main/java/com/example/demo/HelloController.java
+++ springboot-devops-app/src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,14 @@
 package com.example.demo;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-public class HelloController{
-@GetMapping("/") public String hello(){return "Hello from DevOps GitOps Pipeline!";}
-@GetMapping("/health") public String health(){return "Application Running Successfully";}
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Spring Boot CI/CD Project";
+    }
+
 }
