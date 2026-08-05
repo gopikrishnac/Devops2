af0c49b5447fdb1464f3c0856d798a158c298f0b
Gopikrishna <chodapaneedigopikrishna@gmail.com>
2026-08-05 17:14:53 +0530

    change content

--- springboot-devops-app/src/main/java/com/example/demo/HelloController.java
+++ springboot-devops-app/src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Spring Boot CI/CD Project";
+        return "Hello This Gopikrishna";
     }
 
 }
